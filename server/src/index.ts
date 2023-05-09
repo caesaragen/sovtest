2157bc587600beafa5bfb5bc2b3a3412cb1de7fc
Joel Aduma <caesaragen@gmail.com>
2023-05-09 19:52:21 +0300

    use express server and cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,11 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { expressMiddleware } from '@apollo/server/express4';
+import http from 'http';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import express from 'express';
 import typeDefs from './graphql/schema/schema.ts';
 import resolvers from './graphql/resolvers/resolvers.ts';
 import StarWarsAPI from './datasources/starwars-api.ts';
@@ -10,21 +16,33 @@ interface ContextValue {
     };
 }
 
+const app = express();
+const httpServer = http.createServer(app);
+app.use(cors())
 const server = new ApolloServer<ContextValue>({
     typeDefs,
     resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
+await server.start();
 
-const { url } = await startStandaloneServer(server, {
-    context: async () => {
-        const { cache } = server;
-        return {
-
-            dataSources: {
-                starWarsAPI: new StarWarsAPI(),
-            },
-        };
-    },
-});
-
-console.log(`Server running at ${url}`);
+app.use(
+    '/',
+    cors<cors.CorsRequest>(),
+    bodyParser.json(),
+    expressMiddleware(server, {
+        context: async () => {
+            const { cache } = server;
+            return {
+    
+                dataSources: {
+                    starWarsAPI: new StarWarsAPI(),
+                },
+            };
+        },
+    }),
+  );
+  
+  // Modified server startup
+  await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000/`);
